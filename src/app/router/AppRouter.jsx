502bcd7ee9b0b3d17cd19abf980bad3cd9656936
Dennis_Medrano <denniswilson17@hotmail.com>
2026-04-20 22:49:42 -0300

    refactor(arch): move main, estilos e cria router shell (Fase 2)

---
+++ src/app/router/AppRouter.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+export function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* rotas serão adicionadas na Fase 3 */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
