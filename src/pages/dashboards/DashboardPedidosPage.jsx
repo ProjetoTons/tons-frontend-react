5dff45f96a7545c0d45a39d3f09951955ac8df7d
Davi Silva <davi.fsilva@sptech.school>
2026-05-24 16:54:49 -0300

    fix: adiocionar link para dashboard na navbar de pedidos

--- src/pages/dashboards/DashboardPedidosPage.jsx
+++ src/pages/dashboards/DashboardPedidosPage.jsx
@@ -6,11 +6,19 @@ import FilterTabsFeature from '@/features/filter-tabs/ui/FilterTabsFeature';
 import { StageAllocationChart } from '@/widgets/order-charts/StageAllocationChart';
 import { EmployeePerformanceChart } from '@/widgets/order-charts/EmployeePerformanceChart';
 import { DelayedOrdersList } from '@/widgets/delayed-orders-list/DelayedOrdersList';
+import TopNavBar from '@/widgets/topnav-grafica/TopNavBar';
 // import { ServiceStatusSidebar } from '@/widgets/service-status-sidebar';
 
 export default function DashboardPedidosPage() {
+
+    const handleNavClick = (page) => {
+        console.log("Navegando para:", page);
+    };
+
     return (
         <div className="min-h-screen bg-[#F8F9FA] p-8 font-sans text-gray-800">
+
+            <TopNavBar onNavClick={handleNavClick} currentPage="dashboard" />
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
                 <div className="lg:col-span-9 flex flex-col gap-6">
 
@@ -24,12 +32,12 @@ export default function DashboardPedidosPage() {
                     <StatsGrid />
 
                     <StageAllocationChart />
-  
- 
-                     <div>
+
+
+                    <div>
                         <EmployeePerformanceChart />
-                       <DelayedOrdersList />
-                    </div> 
+                        <DelayedOrdersList />
+                    </div>
 
                 </div>
             </div>
